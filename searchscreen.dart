ab4e502affc06a26baf056776a906bb982a2933f
Shing Lee <leesc3@cardiff.ac.uk>
2020-02-20 21:24:50 +0000

    searchscreen 'predictions'

--- searchscreen.dart
+++ searchscreen.dart
@@ -1,3 +1,6 @@
+import 'dart:convert';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SearchScreen extends StatefulWidget {
@@ -6,58 +9,107 @@ class SearchScreen extends StatefulWidget {
 }
 
 class SearchScreenState extends State<SearchScreen> {
-  void _textFieldChanged(String value) async{
-    print(value);
+  bool _searching;
+  String _searchTerm;
+  var _placesPredictions;
+
+  @override
+  void initState() {
+    super.initState();
+    _searching = false;
+  }
+
+  void _textFieldChange(String value) {
+    if (value != '') {
+      setState(() {
+        _searching = true;
+        _searchTerm = value.replaceAll(RegExp(' +'), '+');
+      });
+    } else {
+      setState(() {
+        _searching = false;
+        _searchTerm = '';
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBarWidgetBuilder(context, 'Search Screen'),
-      body: _SearchWidget(context),
+      body: _buildSearchWidget(context),
     );
   }
 
-  Widget _appBarWidgetBuilder(BuildContext context, String titleName){
+  Widget _appBarWidgetBuilder(BuildContext context, String titleName) {
     return AppBar(
       leading: IconButton(
         icon: Icon(Icons.arrow_back),
-        onPressed: () {Navigator.pop(context);},
+        onPressed: () {
+          Navigator.pop(context);
+        },
       ),
       title: Text('$titleName'),
     );
   }
 
-  Widget _SearchWidget(BuildContext context){
-    return Container (
-        child: Stack(
-            children: <Widget>[
-              Container(
-                color: Color(0xFF000000),
-                child: Center(
-                  child: Text(
-                    'MAP GOES HERE',
-                    style: TextStyle(
-                      color: Color(0xFFFFFFFF),
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ),
-              ), Padding(
-                padding: EdgeInsets.fromLTRB(5, 5, 5, 0),
-                child: Card(
-                    child: TextField(
-                      onChanged: _textFieldChanged,
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Search",
-                        suffixIcon: Icon(Icons.search),
-                      ),
-                    )
-                ),
-              )
-            ]
-        )
+  Widget _buildSearchWidget(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        _backgroundWidget(context),
+        Card(
+          child:Column(
+            mainAxisSize: MainAxisSize.min,
+            children: _searchWidgetList(),
+          ),
+        ),
+      ],
     );
+  }
+
+  Widget _backgroundWidget(BuildContext context) {
+    return Container(
+      color: Colors.black,
+      child: Center(
+        child: Text(
+          'Map Goes Here',
+          style: TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  List<Widget> _searchWidgetList() {
+    var builder = <Widget>[
+      new TextField(
+        onChanged: _textFieldChange,
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: 'Search',
+          suffixIcon: Icon(Icons.search),
+        )
+      ),
+    ];
+    if (_searching) {
+      builder.add(
+        new Flexible(
+          child: new ListView.separated(
+            itemCount: 1,
+            itemBuilder: (BuildContext context, int index){
+              return new Container(
+                child: new Text('$_searchTerm'),
+              );
+            },
+            separatorBuilder: (BuildContext context, int index){
+              return new Divider();
+            },
+          ),
+        )
+      );
+    }
+    return builder;
   }
 }
