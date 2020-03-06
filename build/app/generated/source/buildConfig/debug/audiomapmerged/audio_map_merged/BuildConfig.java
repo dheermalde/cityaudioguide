cfa74c7937d66bb04d33d169b6d160f41bcb8394
Shing Lee <leesc3@cardiff.ac.uk>
2020-03-06 16:15:11 +0000

    v2 merged functionalities

---
+++ build/app/generated/source/buildConfig/debug/audiomapmerged/audio_map_merged/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package audiomapmerged.audio_map_merged;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "audiomapmerged.audio_map_merged";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
